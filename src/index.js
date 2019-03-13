d41852a20cb368f4629e1deef9bb5e2b7cb2881f
Yulia Beliak <belyakzhe@gmail.com>
2019-03-13 17:29:50 +0000

    feat: implement function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    // your implementation
+    let count = 0;
+    for (let i = 0; i < preferences.length; i++) {
+        let firstlove = preferences[i] - 1;
+        if (firstlove == i) continue;
+        let secondlove = preferences[firstlove] - 1;
+        if (secondlove == firstlove) continue;
+        let thirdlove = preferences[secondlove] - 1;
+        if (thirdlove == secondlove) continue;
+        if (thirdlove != i) continue;
+        count++;
+    }
+    return count / 3;
 };
